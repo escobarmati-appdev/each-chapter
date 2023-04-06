c61f086acc7b5dd4d3014233c9b64ca5bb056256
Matias Escobar <mescobar@chicagobooth.edu>
2023-04-06 05:33:10 +0000

    Each HW

--- each_even_word.rb
+++ each_even_word.rb
@@ -3,4 +3,20 @@
 #   The program should only print the words that are have an even number of characters
 # Look in the github README file for example output
 
+
+#Example (mountain pink vines is the input)
+#"Enter a list of words separated by spaces:"
+#mountain pink vines
+#"mountain"
+#"pink"
+
 p "Enter a list of words separated by spaces:"
+
+input = gets.chomp
+words = input.split(" ")
+
+words.each do |word|
+  if word.length.even?
+    p word
+  end
+end
--- each_spell_word.rb
+++ each_spell_word.rb
@@ -3,5 +3,19 @@
 # Asks your user to enter any word and have it spelled out letter by letter.  
 # Look in the github README file for example output
 
-p "Enter a word for me to spell:"
+#"Enter a word for me to spell:"
+#Georgia
+#"G"
+#"E"
+#"O"
+#"R"
+#"G"
+#"I"
+#"A"
 
+p "Enter a word for me to spell:"
+word = gets.chomp
+
+word.split("").each do |letter|
+  p letter.upcase
+end
